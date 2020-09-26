f1c326df79136d3d358fbdd94d7f4faf789334a3
Jesse <jesse@milette.online>
2020-09-26 11:19:57 -0400

    Fix env var not being applied

--- buildme.sh
+++ buildme.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-DOCKER_CLI_EXPERIMENTAL=enabled
+export DOCKER_CLI_EXPERIMENTAL=enabled
 docker buildx build -t jmilette/apache-phpfpm:dev-18.04 --platform=linux/arm,linux/arm64,linux/amd64 . --push
