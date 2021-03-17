932a02c07cdc1e395ffe55edcac89bdb24d76264
Jesse <jesse@milette.online>
2021-03-17 10:53:43 -0400

    Create dockerx builder, fixes #3

--- buildme.sh
+++ buildme.sh
@@ -2,7 +2,13 @@
 
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
+echo "Resetting builder..."
+docker run --rm --privileged multiarch/qemu-user-static:5.2.0-1 --reset -p yes
+docker buildx rm multiarch
+docker buildx create --name multiarch --driver docker-container --use
+docker buildx inspect --bootstrap
+
 echo "Building for PHP8..."
-docker buildx build -t jmilette/apache-phpfpm:php-8.0 --build-arg PHPVERSION=8.0 --platform=linux/arm,linux/arm64,linux/amd64 . --push
+docker buildx build -t jmilette/apache-phpfpm:php-8.0 --build-arg PHPVERSION=8.0 --platform=linux/armhf,linux/arm64,linux/amd64 . --push
 echo "Building for PHP7.4..."
-docker buildx build -t jmilette/apache-phpfpm:php-7.4 --build-arg PHPVERSION=7.4 --platform=linux/arm,linux/arm64,linux/amd64 . --push
+docker buildx build -t jmilette/apache-phpfpm:php-7.4 --build-arg PHPVERSION=7.4 --platform=linux/armhf,linux/arm64,linux/amd64 . --push
